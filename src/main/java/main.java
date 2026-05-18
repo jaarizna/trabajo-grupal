22834b3652f4e6bcc4664d05cdefc9164fe30a05
ravillam1 <91558075+ravillam1@users.noreply.github.com>
2026-05-18 20:24:42 +0200

    main

---
+++ src/main/java/main.java
@@ -0,0 +1,24 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.Dominio.Criatura;
+import main.java.LogicaNegocio.Simulacion;
+
+public class main {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();	
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        System.out.println("LISTA= "+a);
+
+        Simulacion s=new Simulacion(1, a);
+
+        s.simular();
+        for (String res : s.getResultados()) {
+            System.out.println(res);
+        }
+    }
+}
